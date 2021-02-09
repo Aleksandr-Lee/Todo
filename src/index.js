5b5577c51cd54efe328a350863225e847d323d97
Aleksandr-Lee <71881684+Aleksandr-Lee@users.noreply.github.com>
2021-02-09 14:02:23 +0300

    Заголовок

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
+import "./index.css";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>todos</h1>
+      <input
+        className="new-todo"
+        placeholder="What needs to be done?"
+        autofocus
+      />
+    </header>
+  );
+};
+
+ReactDOM.render(<Header />, document.getElementById("root"));
