dff666df6ed501e64ad37b42c1cc17694a774b4b
Aleksandr-Lee <71881684+Aleksandr-Lee@users.noreply.github.com>
2021-02-11 16:20:19 +0300

    редактирование компонентов

--- src/components/task-list.js
+++ src/components/task-list.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Task from "./task";
 
-const TaskList = () => {
-	return (
-		<section className="main">
-     <Task  />
+const TaskList = (props) => {
+  const element = props.task.map((item) => {
+    return <Task task={item.task} key={item.id} />;
+  });
+
+  return (
+    <section className="main">
+      <ul className="todo-list">{element}</ul>
     </section>
-	);
-}
+  );
+};
 
 export default TaskList;
--- src/index.js
+++ src/index.js
@@ -3,21 +3,21 @@ import ReactDOM from "react-dom";
 
 import Header from "../src/components/header";
 import TaskList from "./components/task-list";
-import Footer from '../src/components/footer'
+import Footer from "../src/components/footer";
 
 import "./index.css";
 
 const App = () => {
-	// const todoData = [
-	// 	{label: 'Completed task'}
-	// 	{label: 'Editing task'}
-	// 	{label: 'Active task'}
-	// ]
+  const tasks = [
+    { id: 1, task: "Completed task", class: "completed" },
+    { id: 2, task: "Editing task", class: "editing" },
+    { id: 3, task: "Active task", class: "" }
+  ];
   return (
     <div>
       <Header />
-      <TaskList />
-		<Footer />
+      <TaskList task={tasks} />
+      <Footer />
     </div>
   );
 };
