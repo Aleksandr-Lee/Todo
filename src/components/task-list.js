2c5fe8d9489e458c323998ed8f8891420957c9f4
Aleksandr Lee <lexxlee@rambler.ru>
2021-02-11 23:39:03 +0300

    добавил время создания задачи

--- src/components/task-list.js
+++ src/components/task-list.js
@@ -1,12 +1,23 @@
 import React from "react";
 import Task from "./task";
 
-const TaskList = () => {
-	return (
-		<section className="main">
-     <Task  />
+const TaskList = (props) => {
+  const element = props.task.map((item) => {
+    return (
+      <Task
+        task={item.task}
+        key={item.id}
+        created={item.created}
+        class={item.class}
+      />
+    );
+  });
+
+  return (
+    <section className="main">
+      <ul className="todo-list">{element}</ul>
     </section>
-	);
-}
+  );
+};
 
 export default TaskList;
--- src/index.js
+++ src/index.js
@@ -3,21 +3,26 @@ import ReactDOM from "react-dom";
 
 import Header from "../src/components/header";
 import TaskList from "./components/task-list";
-import Footer from '../src/components/footer'
+import Footer from "../src/components/footer";
 
 import "./index.css";
 
 const App = () => {
-	// const todoData = [
-	// 	{label: 'Completed task'}
-	// 	{label: 'Editing task'}
-	// 	{label: 'Active task'}
-	// ]
+  const tasks = [
+    {
+      id: 1,
+      task: "Completed task",
+      class: false,
+      created: new Date() - 1020000,
+    },
+    { id: 2, task: "Editing task", class: true, created: new Date() - 1 },
+    { id: 3, task: "Active task", class: false, created: new Date() - 300000 },
+  ];
   return (
     <div>
       <Header />
-      <TaskList />
-		<Footer />
+      <TaskList task={tasks} />
+      <Footer />
     </div>
   );
 };
