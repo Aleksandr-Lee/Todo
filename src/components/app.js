2139144558d8fefde91fc6034e6d8b2e90e59db6
Aleksandr Lee <lexxlee@rambler.ru>
2021-03-01 20:05:49 +0300

    add eslint prettier husky lint-staged

--- src/components/app.js
+++ src/components/app.js
@@ -94,7 +94,7 @@ class App extends React.Component {
 
     this.addTask = (text) => {
       const newTask = {
-        id: (this.newId + 1),
+        id: this.newId + 1,
         task: text,
         completed: false,
         editing: false,
